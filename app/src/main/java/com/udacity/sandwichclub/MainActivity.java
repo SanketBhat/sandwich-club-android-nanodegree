b50d0f4401e6dc62ef21ed86d3438e69cdf2afa8
SanketBhat <sanket.r.bhat.u@gmail.com>
2018-10-24 16:36:17 +0530

    Material touch

--- app/src/main/java/com/udacity/sandwichclub/MainActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/MainActivity.java
@@ -2,37 +2,59 @@ package com.udacity.sandwichclub;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.udacity.sandwichclub.adapter.SandwichListAdapter;
+import com.udacity.sandwichclub.model.Sandwich;
+import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.udacity.sandwichclub.DetailActivity.EXTRA_SANDWICH;
+
+public class MainActivity extends AppCompatActivity implements SandwichListAdapter.SandwichClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] sandwiches = getResources().getStringArray(R.array.sandwich_names);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, sandwiches);
+        String[] sandwichStrings = getResources().getStringArray(R.array.sandwich_details);
+        List<Sandwich> sandwiches = getSandwiches(sandwichStrings);
 
-        // Simplification: Using a ListView instead of a RecyclerView
-        ListView listView = findViewById(R.id.sandwiches_listview);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                launchDetailActivity(position);
-            }
-        });
+        RecyclerView recyclerView = findViewById(R.id.sandwich_list);
+        SandwichListAdapter adapter = new SandwichListAdapter(this, sandwiches, this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(adapter);
+
     }
 
-    private void launchDetailActivity(int position) {
+    private List<Sandwich> getSandwiches(String[] sandwichStrings) {
+        ArrayList<Sandwich> sandwiches = new ArrayList<>();
+        for (String sandwichString :
+                sandwichStrings) {
+            Sandwich sandwich = JsonUtils.parseSandwichJson(sandwichString);
+            sandwiches.add(sandwich);
+        }
+        return sandwiches;
+    }
+
+    private void launchDetailActivity(Sandwich sandwich, ImageView imageView) {
+        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, imageView, getString(R.string.transition_name));
         Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(DetailActivity.EXTRA_POSITION, position);
-        startActivity(intent);
+        intent.putExtra(EXTRA_SANDWICH, sandwich);
+        startActivity(intent, options.toBundle());
     }
+
+    @Override
+    public void onClick(Sandwich sandwich, ImageView imageView) {
+        launchDetailActivity(sandwich, imageView);
+    }
+
 }
